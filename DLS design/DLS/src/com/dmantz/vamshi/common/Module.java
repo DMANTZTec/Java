c890a0521aee21a866e7152190ac52069a96ed44
Vamshi1233338 <bhairojuvamshi01444@gmail.com>
2021-10-22 14:48:43 +0530

    design

---
+++ DLS design/DLS/src/com/dmantz/vamshi/common/Module.java
@@ -0,0 +1,29 @@
+package com.dmantz.vamshi.common;
+
+import java.util.ArrayList;
+
+public class Module {
+	private int moduleId;
+	private String moduleName;
+	private ArrayList<Topic> topics;
+	
+	public int getModuleId() {
+		return moduleId;
+	}
+	public void setModuleId(int moduleId) {
+		this.moduleId = moduleId;
+	}
+	public String getModuleName() {
+		return moduleName;
+	}
+	public void setModuleName(String moduleName) {
+		this.moduleName = moduleName;
+	}
+	public ArrayList<Topic> getTopics() {
+		return topics;
+	}
+	public void setTopics(ArrayList<Topic> topics) {
+		this.topics = topics;
+	}
+
+}
---
+++ DLS design/DLS/src/com/dmantz/vamshi/managers/CourseManager.java
@@ -0,0 +1,21 @@
+package com.dmantz.vamshi.managers;
+
+import com.dmantz.vamshi.common.Course;
+
+public class CourseManager 
+{
+	
+	public void addCourse(Course course)
+	{
+		//add course to the database
+	}
+	public void deleteCourse(Course course)
+	{
+		//delete course from the database
+	}
+	public void assignCourse(Course course)
+	{
+		//assigning courses
+	}
+
+}
